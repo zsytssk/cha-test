8e53638a8071bd02e3c81b450908d2e5483a59d3
shiyangzhang <shiyangzhang@whale.im>
2024-01-05 17:16:51 +0800

    [master]

---
+++ doc/note.md
@@ -0,0 +1,6 @@
+https://pest.rs/book/
+https://pest.rs/#editor
+
+## 2024-01-04 16:50:26
+
+- pest 没有类型说明就像是一个黑魔法
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-  println!("Hello, world!");
+use lazy_static::lazy_static;
+use pest::iterators::Pairs;
+use pest::pratt_parser::{Assoc, Op, PrattParser};
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "demo.pest"]
+struct MyParser;
+
+lazy_static! {
+  static ref PRATT_PARSER: PrattParser<Rule> = {
+    use Assoc::*;
+    use Rule::*;
+
+    PrattParser::new()
+      .op(Op::infix(add, Left) | Op::infix(subtract, Left))
+      .op(Op::infix(multiply, Left) | Op::infix(divide, Left))
+      .op(Op::infix(power, Right))
+  };
 }
+
+fn eval(expression: Pairs<Rule>) -> f64 {
+  PRATT_PARSER
+    .map_primary(|primary| match primary.as_rule() {
+      Rule::num => primary.as_str().parse::<f64>().unwrap(),
+      Rule::expr => eval(primary.into_inner()),
+      _ => unreachable!(),
+    })
+    .map_infix(|lhs, op, rhs| match op.as_rule() {
+      Rule::add => lhs + rhs,
+      Rule::subtract => lhs - rhs,
+      Rule::multiply => lhs * rhs,
+      Rule::divide => lhs / rhs,
+      Rule::power => lhs.powf(rhs),
+      _ => unreachable!(),
+    })
+    .parse(expression)
+}
+
+fn main() {}
